fac49ea1c584bd9cb5806e9a10544654afa6e7b2
ParkDoHyeon <ehgus8910@gmail.com>
2024-04-11 15:39:56 +0900

    first commit

---
+++ out/production/TEAM-1-GITHUB-PRACTICE/c_practice.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    
+    // 사용자로부터 숫자 입력 받기
+    printf("첫 번째 숫자를 입력하세요: ");
+    scanf("%lf", &num1);
+    
+    printf("두 번째 숫자를 입력하세요: ");
+    scanf("%lf", &num2);
+    
+    // 두 숫자를 더하고 결과 출력
+    result = num1 + num2;
+    printf("두 숫자의 합은: %lf\n", result);
+    
+    return 0;
+}
